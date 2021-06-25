3a51858b2f813216f4d3f1ca664eb070e18cf4c0
Yemiluna <yemiluna@gmail.com>
2021-06-25 10:49:40 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints alphabets from a to z
+ * Return: 0 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
