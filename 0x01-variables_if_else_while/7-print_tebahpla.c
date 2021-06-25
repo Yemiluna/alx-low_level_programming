9fd9f6d2c994f78a18b8528e07a15d2ceff4d8aa
Yemiluna <yemiluna@gmail.com>
2021-06-25 10:54:45 -0700

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints alphabets from a to z
+ * Return: 0 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
