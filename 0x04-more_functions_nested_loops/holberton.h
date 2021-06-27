bbeb1f096a9d73e60ef2745a5fd1f8e8e35b3acf
Yemiluna <yemiluna@gmail.com>
2021-06-27 10:12:12 -0700

    holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,8 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(void);
