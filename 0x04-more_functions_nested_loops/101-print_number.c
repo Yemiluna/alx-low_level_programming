bfc139aa345c959ae2ab7ca953c9519aa3ee5fd1
Yemiluna <yemiluna@gmail.com>
2021-06-27 12:33:07 -0700

    101-print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,26 @@
 #include "holberton.h"
 
 /**
- * print_number - print number
- * @n: input value
- *
- * Description:  prints an integer
- * Return: Always (0)
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
-
 void print_number(int n)
 {
-if ((n < 0) && (n >= -9))
-{
-_putchar('-');
-_putchar((n * -1) + '0');
-}
-else if (n <= -10)
-{
-_putchar('-');
-_putchar(((n / 10) * -1) + '0');
-_putchar(((n % 10)  * -1) + '0');
-}
-else if (n == 0)
-{
-_putchar(n + '0');
-}
-else if ((n > 0) && (n <= 9))
-{
-_putchar(n + '0');
-}
-else if ((n >= 10) && (n <= 99))
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-}
-else if ((n >= 100) && (n <= 999))
-{
-_putchar((n / 100) + '0');
-_putchar(((n / 10) % 10) + '0');
-_putchar((n % 10) + '0');
-}
-else if ((n >= 1000) && (n <= 9999))
-{
-_putchar((n / 1000) + '0');
-_putchar((n / 100) % 10 + '0');
-_putchar((n / 10) % 10 + '0');
-_putchar((n % 10) + '0');
-}
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
