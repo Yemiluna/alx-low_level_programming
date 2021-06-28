1be4f000b3927cbca03125c67f992b5912c4ddf6
Yemiluna <yemiluna@gmail.com>
2021-06-28 12:42:10 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - it reset numbers to 98
+ *@n: an integer value,
+ *Return: returns nothing in void cases
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
