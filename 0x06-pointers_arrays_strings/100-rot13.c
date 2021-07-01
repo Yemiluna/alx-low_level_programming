0fe505bbaddcc7c0b936b60d8d11937e9d3a1a7d
Yemiluna <yemiluna@gmail.com>
2021-07-01 13:08:22 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *rot13 - a function that encodes a string using rot13
+ *@s: a string to be encoded
+ *Return: returns a value
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char cod[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alph[j])
+			{
+				s[i] = cod[j];
+				break;
+			}
+		}
+		j = 0;
+	}
+	return (s);
+}
