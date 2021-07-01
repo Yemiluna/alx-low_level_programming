0b0ec6fd17eceb1b5d0805ab56c7506afe4ce5ee
Yemiluna <yemiluna@gmail.com>
2021-07-01 13:11:27 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *print_number - a function to print integer
+ *@n: an integer value
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
