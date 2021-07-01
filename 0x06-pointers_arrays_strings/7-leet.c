58612d7de9a9d6f343694b93c3339a52dc470c41
Yemiluna <yemiluna@gmail.com>
2021-07-01 13:05:29 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *leet - a function that encodes to 1337
+ *@s: a string
+ *Return: returns a character type.
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char l[] = "ol_ea__t";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
+}
