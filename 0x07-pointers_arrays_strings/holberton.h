fae0b5e8e2b958602e737c0f4834a683a5ba1e14
Yemiluna <yemiluna@gmail.com>
2021-07-05 13:15:50 -0700

    holberton

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+int _putchar(char);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
