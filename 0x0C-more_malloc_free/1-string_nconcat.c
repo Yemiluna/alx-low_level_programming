5d237ab7c1fd5a1fe846c49fd554f9d9287bbc1f
Yemiluna <yemiluna@gmail.com>
2021-07-16 01:46:16 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,39 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: first n bytes of s2
- * Return: void pointer
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, len = 0, lens1;
-char *str;
+	char *p;
+	unsigned int t1, t2, i;
 
-if (s1 != NULL)
-for (i = 0; s1[i] != '\0'; i++)
-len++;
-lens1 = len;
-if (s2 != NULL)
-{
-if (sizeof(s2) > n)
-for (i = 0; i < n; i++)
-len++;
-else
-for (i = 0; s2[i] != '\0'; i++)
-len++;
+	if (s1 == NULL)
+		t1 = 0;
+	for (t1 = 0; s1[t1]; ++t1)
+		;
+	if (s2 == NULL)
+		t2 = 0;
+	for (t2 = 0; s2[t2]; ++t2)
+		;
+	if (t2 > n)
+		t2 = n;
+	p = malloc((t1 + t2 + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < t1; i++)
+		p[i] = s1[i];
+	for (i = 0; i < t2; i++)
+		p[t1 + i] = s2[i];
+	p[t1 + t2] = '\0';
+	return (p);
 }
-str = malloc(sizeof(char) * (len + 1));
-if (str == NULL)
-return (NULL);
-str[len] = '\0';
-if (s1 != NULL)
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-if (s2 != NULL)
-for (; i < len; i++)
-str[i] = s2[i - lens1];
-return (str);
-}
+
 
