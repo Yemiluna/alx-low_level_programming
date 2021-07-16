f4e2560c042550ebd7ea0b7eea0511f9c6f0591a
Yemiluna <yemiluna@gmail.com>
2021-07-16 01:30:40 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,42 @@
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - concatenate two strings
- *
- * @s1: the first string
- * @s2: the second string
- * @n: number of bytes from @s2 to join with @s1
- *
- * Description: if NULL is passed, treat as empty string
- *		if @n > length of @s2, take the whole string
- *
- * Return: pointer to the concatenated string, otherwise NULL
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: first n bytes of s2
+ * Return: void pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, i, j;
-	char *s;
-	char *nul = "";
+unsigned int i, len = 0, lens1;
+char *str;
 
-	if (s1 == NULL)
-		s1 = nul;
-	if (s2 == NULL)
-		s2 = nul;
-
-	len1 = 0, len2 = 0;
-	while (*(s1 + len1))
-		len1++;
-	while (*(s2 + len2))
-		len2++;
-
-	if (n < len2)
-		len2 = n;
-
-	s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (s == 0)
-		return (0);
-
-	for (i = 0; i < len1; i++)
-		*(s + i) = *(s1 + i);
-
-	for (i = 0, j = len1; i <= len2; j++, i++)
-		*(s + j) = *(s2 + i);
-	*(s + j) = '\0';
-
-	return (s);
+if (s1 != NULL)
+for (i = 0; s1[i] != '\0'; i++)
+len++;
+lens1 = len;
+if (s2 != NULL)
+{
+if (sizeof(s2) > n)
+for (i = 0; i < n; i++)
+len++;
+else
+for (i = 0; s2[i] != '\0'; i++)
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+str[len] = '\0';
+if (s1 != NULL)
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
+if (s2 != NULL)
+for (; i < len; i++)
+str[i] = s2[i - lens1];
+return (str);
 }
 
