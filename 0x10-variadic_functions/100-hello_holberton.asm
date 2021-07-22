6bc3739a24d8f96b3ba4b87431bf897194816896
Yemiluna <yemiluna@gmail.com>
2021-07-22 15:19:48 -0700

    100-hello_holberton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
+	msg db "Hello, Holberton",10
+
+	section .text
 	global main
 
 main:
-	mov rax, 4
-	mov rbx, 1
-	mov rcx, holbiemessage
-	mov rdx, lengthmessage
-	int 80h
-
 	mov rax, 1
-	mov rbx, 0
-	int 80h
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
 
-holbiemessage:		db "Hello, Holberton", 10
-lengthmessage:		equ $ - holbiemessage
+	mov rax, 60
+	mov rdi, 0
+	syscall
