aea96b1c9af8c23eacadbc5daa13392462dc2c9e
Yemiluna <yemiluna@gmail.com>
2021-08-06 02:14:14 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,8 @@
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 
 #endif
